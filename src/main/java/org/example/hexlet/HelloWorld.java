60c8325011e42677cf196a8b3db58d99d5eb160e
Prototype206 <ulyanow.mischa2017@yandex.ru>
2026-04-22 16:12:18 +0400

    add dynamic routes

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -13,11 +13,24 @@ public class HelloWorld {
 
         app.post("/users", ctx -> ctx.result("POST /users"));
         app.get("/users", ctx -> ctx.result("GET /users"));
-
         app.get("/hello", ctx -> {
             var name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
             ctx.result("Hello, " + name + "!");
         });
+        app.get("/courses/{courseId}/lessons/{id}", ctx -> {
+            var courseId = ctx.pathParam("courseId");
+            var lessonId =  ctx.pathParam("id");
+            ctx.result("Course ID: " + courseId + " Lesson ID: " + lessonId);
+        });
+        app.get("/courses/{id}", ctx -> {ctx.result("Course ID: " + ctx.pathParam("id"));});
+        app.get("/users/{id}", ctx -> {ctx.result("User ID: " + ctx.pathParam("id"));});
+
+        app.get("/users/{id}/post/{postId}", ctx -> {
+            var id = ctx.pathParam("id");
+            var postId = ctx.pathParam("postId");
+            ctx.result("Id = " + id + " + postId = " + postId);
+            });
+
         app.start(7070);
     }
 }
