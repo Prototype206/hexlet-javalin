ac2cfad6b7103525d21c9e2f61d0684b0782178e
Prototype206 <ulyanow.mischa2017@yandex.ru>
2026-04-22 12:36:59 +0400

    added a greeting using the request parameters

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,13 +1,23 @@
 package org.example.hexlet;
 
 import io.javalin.Javalin;
+import io.javalin.plugin.bundled.DevLoggingPlugin;
+
 
 public class HelloWorld {
     public static void main(String[] args) {
         var app = Javalin.create(config -> {
-            config.bundledPlugins.enableDevLogging();
+            config.registerPlugin(new DevLoggingPlugin());
+            //config.bundledPlugins.enableDevLogging();
         });
-        app.get("/", ctx -> ctx.result("Hello World"));
+
+        app.post("/users", ctx -> ctx.result("POST /users"));
+        app.get("/users", ctx -> ctx.result("GET /users"));
+
+        app.get("/hello", ctx -> {
+            var name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
+            ctx.result("Hello, " + name + "!");
+        });
         app.start(7070);
     }
 }
